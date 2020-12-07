e457edc1fcb2efda9cfdc73c19c8e34658d89024
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2020-12-07 15:50:35 +0000

    PA2 photo URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root')
+
+const state = {eventCount: 0, username: ''}
 
 function App() {
+  
+  function handleClick() {
+    setState({eventCount: state.eventCount + 1})
+  }
+
+  function handleChange(event) {
+    setState({photo: event.target.value})
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <p>Provide URL for Building Photo: </p>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input onChange={handleChange} />
         </p>
+
+        <p>The building is classified as: [built vs. construction] </p>
+        
+        <p>
+          <img src={state.photo} height="200"/>
+        </p>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/HBAkirmak/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Twitter: @HBAkirmak
         </a>
       </header>
     </div>
   );
 }
 
+function setState(newState) {
+  Object.assign(state, newState)
+  renderApp()
+}
+
+function renderApp() {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+    
 export default App;
