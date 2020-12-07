c9e849d2a65144fa87e96ca82d0cea9f03f6e277
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2020-12-07 17:25:34 +0000

    PA3 API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 
 const rootElement = document.getElementById('root')
 
@@ -14,6 +16,21 @@ function App() {
 
   function handleChange(event) {
     setState({photo: event.target.value})
+    
+    const api = 'https://g1sl8z5hse.execute-api.us-east-1.amazonaws.com/Prod/invocations';
+    const data = { url : 'https://mkenyaujerumani.de/wp-content/uploads/2016/09/House-under-construction.jpg' };
+
+    axios
+      .post(api, data)
+      .then((response) => {
+        setState({predictionResp: response});
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
+
   }
   
   return (
@@ -28,7 +45,7 @@ function App() {
         </p>
 
         <p>The building is classified as: [built vs. construction] </p>
-        
+        <p>predictionResp: {state.predictionResp} </p>
         <p>
           <img src={state.photo} height="200"/>
         </p>
